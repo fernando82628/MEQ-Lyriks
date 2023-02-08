60df05a3f687751e4ec0170fabfa63d342d9540d
Fernando <fernando82628@yahoo.com>
2023-02-07 21:51:27 -0600

    2nd commit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,20 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+    console.log(genres);
+
+    return (
+        <div className='flex flex-col'>
+            <div className='w-full flex justify-between items-center
+            sm:flex-fow flex-col mt-4 mb-10'>
+                <select>
+                    
+                </select>
+            </div>
+
+        </div>
+    );
+};
 
 export default Discover;
