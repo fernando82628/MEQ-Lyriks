316dd71f22bd02866fff157e2aef6a2ad06a7258
Fernando <fernando82628@yahoo.com>
2023-02-08 18:40:11 -0600

    Made correciton in builder query /charts/track

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,17 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
- 
-  fetch('https://shazam.p.rapidapi.com/charts/track', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
-    export const shazamCoreApi = createApi({
+       export const shazamCoreApi = createApi({
         reducerPath: 'shazamCoreApi',
         baseQuery: fetchBaseQuery({
             baseUrl: 'https://shazam.p.rapidapi.com',
-            prepareHeaders: () => {
-                headers.set('X-RapidAPI-Key', 'e4d1d6ebcdmshbc44b89ef25891cp1d4570jsn681c785b8fb7')
-            }
+            prepareHeaders: (headers) => {
+                headers.set('X-RapidAPI-Key', 'e4d1d6ebcdmshbc44b89ef25891cp1d4570jsn681c785b8fb7');
+
+                return headers;
+            },
+        }),
+        endpoints: (builder) => ({
+            getTopCharts: builder.query({ query: '/charts/track' }),
         })
     });
+
+    export const {
+        useGetTopChartsQuery,
+    } = shazamCoreApi;
