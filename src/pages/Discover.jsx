39baa78d81bbcc4ab436f14d2721b6c5dba0b61e
Fernando <fernando82628@yahoo.com>
2023-02-09 19:22:46 -0600

    modified shazamCore a bit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,12 +1,14 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
-
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+    const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
 
-    return (
+    if()
+
+     return (
         <div className='flex flex-col'>
             <div className='w-full flex justify-between items-center
             sm:flex-fow flex-col mt-4 mb-10'>
@@ -24,7 +26,7 @@ const Discover = () => {
             </div>
             <div className='flex flex-wrap sm:justify-start
                 justify-center gap-8'>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+                {data?.map((song, i) => (
                     <SongCard
                         key={song.key}
                         song={song}
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,20 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-       export const shazamCoreApi = createApi({
-        reducerPath: 'shazamCoreApi',
-        baseQuery: fetchBaseQuery({
-            baseUrl: 'https://shazam.p.rapidapi.com',
-            prepareHeaders: (headers) => {
-                headers.set('X-RapidAPI-Key', 'e4d1d6ebcdmshbc44b89ef25891cp1d4570jsn681c785b8fb7');
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam.p.rapidapi.com',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'e4d1d6ebcdmshbc44b89ef25891cp1d4570jsn681c785b8fb7');
 
-                return headers;
-            },
-        }),
-        endpoints: (builder) => ({
-            getTopCharts: builder.query({ query: '/charts/track' }),
-        })
-    });
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        //Line 15 is 'building' api endpoints we want to call.
+        getTopCharts: builder.query({ query: () => '/charts/track' }),
+    }),
+});
 
-    export const {
-        useGetTopChartsQuery,
-    } = shazamCoreApi;
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
